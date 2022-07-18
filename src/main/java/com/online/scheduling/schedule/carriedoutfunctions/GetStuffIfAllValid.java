1b8398e3899cf7bbe663686b8514b6ca3f9e0bda
Lucky1Man <86126779+Lucky1Man@users.noreply.github.com>
2022-07-19 00:10:10 +0300

    Update GetStuffIfAllValid.java

--- src/main/java/com/online/scheduling/schedule/carriedoutfunctions/GetStuffIfAllValid.java
+++ src/main/java/com/online/scheduling/schedule/carriedoutfunctions/GetStuffIfAllValid.java
@@ -7,13 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetStuffIfAllValid {
+    private static final String defaultName = "planned stuff";
 
-    public static <T extends IPlannedStuff<T>> List<T> getStuffIfAllValid(List<T> validated, IPlannedStuffValidator<T> stuffValidator){
+    public static <T extends IPlannedStuff<T>>
+    List<T> getStuffIfAllValid(List<T> validated, IPlannedStuffValidator<T> stuffValidator, String plannedStuffName){
         List<T> result = new ArrayList<>();
         if(stuffValidator.validateAll(validated)){
+            for(var val : validated){
+                if(val.getId() != null)
+                    throw new UnableToExecuteQueryException("You can not specify %s id when trying to save it".formatted(plannedStuffName));
+            }
             result.addAll(validated);
             return result;
         }
         return result;
+    }
+
+    public static <T extends IPlannedStuff<T>>
+    List<T> getStuffIfAllValid(List<T> validated, IPlannedStuffValidator<T> stuffValidator){
+        return getStuffIfAllValid(validated, stuffValidator, defaultName);
     }
 }
